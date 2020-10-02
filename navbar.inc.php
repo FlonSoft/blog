b215f6ce80fc0d668fdf0fe9c23d64535cd8b86f
Flolon Fox <me@flolon.cc>
2020-10-01 20:21:41 -0400

    add title to pfp dropdown

--- navbar.inc.php
+++ navbar.inc.php
@@ -5,7 +5,7 @@
                 <span class="flex-grow"></span>
 <?php if($loggedIn) { ?>
                 <a class="item nav-link dp-text " tabindex="0" href="<?= $rootUrl ?>new/" title="New post"><span class="material-icons">add</span></a>
-                <span id="navPfpBtn" class="item nav-pfp" tabindex="0"><img src="<?= $_SESSION['pfp'] ?>" height="45px" alt="<?= $_SESSION['username'] ?>"></span>
+                <span id="navPfpBtn" class="item nav-pfp" tabindex="0" title="Logged in as <?= $_SESSION['username'] ?>"><img src="<?= $_SESSION['pfp'] ?>" height="45px" alt="<?= $_SESSION['username'] ?>"></span>
 <?php }else { ?>
                 <a class="item nav-link <?php echo ($title == 'Sign in') ? 'active' : ''; ?>" tabindex="0" href="<?= $rootUrl ?>auth/"><span>Sign in</span></a>
  <?php } ?>
