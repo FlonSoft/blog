1c96a4773405f44768e492f79629c320cd155f24
Flolon Fox <me@flolon.cc>
2020-10-03 21:03:11 -0400

    change signed in as title

--- navbar.inc.php
+++ navbar.inc.php
@@ -5,7 +5,7 @@
                 <span class="flex-grow"></span>
 <?php if($loggedIn) { ?>
                 <a class="item nav-link dp-text " tabindex="0" href="<?= $rootUrl ?>new/" title="New post"><span class="material-icons">add</span></a>
-                <span id="navPfpBtn" class="item nav-pfp" tabindex="0" title="Logged in as <?= $_SESSION['username'] ?>"><img src="<?= $_SESSION['pfp'] ?>" height="45px" alt="<?= $_SESSION['username'] ?>"></span>
+                <span id="navPfpBtn" class="item nav-pfp" tabindex="0" title="Signed in as <?= $_SESSION['username'] ?>"><img src="<?= $_SESSION['pfp'] ?>" height="45px" alt="<?= $_SESSION['username'] ?>"></span>
 <?php }else { ?>
                 <a class="item nav-link <?php echo ($pageTitle == 'Sign in') ? 'active' : ''; ?>" tabindex="0" href="<?= $rootUrl ?>auth/"><span>Sign in</span></a>
  <?php } ?>
@@ -17,7 +17,7 @@
 <?php if($loggedIn) { ?>
         <div class="container pfp-dropdown-container">
             <nav id="navPfpDropdown" class="pfp-dropdown dropdown" style="display: none;">
-                <div class="dropdown-item"><span class="dp-text"><b><?= $_SESSION['username'] ?></b></span></div>
+                <div class="dropdown-item"><span class="dp-text" title="Signed in as <?= $_SESSION['username'] ?>"><b><?= $_SESSION['username'] ?></b></span></div>
                 <div class="divider"></div>
                 <div class="items">
                     <a class="dropdown-item" href="<?= $rootUrl ?>auth/?action=logout"><span class="dp-text"><span class="material-icons">exit_to_app</span>Sign out</span></a>
